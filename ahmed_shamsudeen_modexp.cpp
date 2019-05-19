5c6d7a21de3a4d58854f44e40b844b1949f2539a
ahmed-28 <49692423+ahmed-28@users.noreply.github.com>
2019-05-19 13:18:55 +0530

    Update ahmed_shamsudeen_modexp.cpp

--- ahmed_shamsudeen_modexp.cpp
+++ ahmed_shamsudeen_modexp.cpp
@@ -11,7 +11,7 @@
         {
             if (exp % 2 == 1)
                 result = (result * base) % mod;
-            exp = exp >> 1;
+            exp = exp/2;
             base = (base * base) % mod;
         }
         return result;
